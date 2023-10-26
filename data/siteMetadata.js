2ac88c3033cb136794bef1ed6183b9b42411277b
S.Alireza Mousavizade <s.a.mosawizade@gmail.com>
2023-10-26 16:46:05 +0330

    add

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,6 +1,6 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'Next.js',
+  title: 'RiskLab AI',
   author: 'S.Alireza Mousavizade',
   headerTitle: 'RiskLabAI',
   description: 'A blog created with Next.js and Tailwind.css',
